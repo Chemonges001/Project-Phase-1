4a355dc4945e924b710e8d142fbfab68b8a3393b
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-24 10:48:09 +0300

    creating functions

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ const cocktailsElement = document.getElementById('cocktails');
 const cocktailOutput= document.getElementById('output-heading');
 const selectedCocktail = document.getElementById('selected-cocktail');
 
+const searchCocktail = (e) => {
+ 
+    e.preventDefault();
+  
+   
+    cocktailsElement.innerHTML = '';
+    selectedCocktail.innerHTML = '';
+  
+   
+    const searchInput = search.value;
+   
