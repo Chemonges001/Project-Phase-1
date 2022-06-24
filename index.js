d76c4587bc8e00c02bca19d6d59c47eb9d78b23f
oscar kiprop chemonges <oscar.chemonges@student.moringaschool.com>
2022-06-24 10:19:01 +0300

    fecthing DOM elements

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const submit = document.getElementById('submit');
+
+const search = document.getElementById('search');
+
+const cocktailsElement = document.getElementById('cocktails');
+const cocktailOutput= document.getElementById('output-heading');
+const selectedCocktail = document.getElementById('selected-cocktail');
+
